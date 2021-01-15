f81674eb258e1b23419b45efc98bb39838ae03e7
Richard Hu <r.hu@berkeley.edu>
2021-01-14 19:18:44 -0800

    Small stuff

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="riznets", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Richard Hu",
     author_email="r.hu@berkeley.edu",
     description="Make and train neural networks on MNIST and CIFAR-10",
